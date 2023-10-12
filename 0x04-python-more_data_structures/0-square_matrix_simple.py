07cd750587ba7242b8f1d70a697686c11173488b
zine22 <kamiljamil15@gmail.com>
2023-10-11 17:11:23 -0700

    sqr

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    val_ = [[] for x in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            val_[i].append(matrix[i][j] ** 2)
+            return val_
