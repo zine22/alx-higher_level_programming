16fdb4a56eb71fb7423aabb5261986d704ffc5c1
zine22 <kamiljamil15@gmail.com>
2024-01-08 13:07:57 -0800

    noc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     noc = ""
-    for i in my_string:
-        if (i == "c") or (i == "C"):
-            noc += ""
-        else:
-            noc += i
-        return noc
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            noc += my_string[i]
+    return noc
