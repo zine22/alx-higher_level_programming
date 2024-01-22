10b7cd154dc03050cfe7b6db46529b1c6b84eaec
zine22 <kamiljamil15@gmail.com>
2024-01-22 07:30:22 -0800

    safe

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/user/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        val = fct(*args)
+        return val
+    except (ValueError, TypeError, ZeroDivisionError, IndexError) as inst:
+        message = "Exception: {}".format(inst)
+        sys.stderr.write(message + "\n")
+        return None
+    
