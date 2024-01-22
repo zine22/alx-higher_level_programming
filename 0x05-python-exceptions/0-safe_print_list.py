c8b8a813166ae96612e4ada253b18fdea8dffd80
zine22 <kamiljamil15@gmail.com>
2024-01-22 03:48:50 -0800

    x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         while a < x:
             print(my_list[a], end='')
-            a+= 1
-    except: IndexError:
+            a += 1
+    except IndexError:
         None
         print()
     return a
