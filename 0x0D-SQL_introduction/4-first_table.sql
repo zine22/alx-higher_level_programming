2df6b8b2c4e41060b4050a8b3bb809594e2c7a4c
zine22 <kamiljamil15@gmail.comgit config --global user.name zine22>
2024-03-12 19:01:32 -0700

    sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- a script that creates a table called first_table in the current database in my MySQL server.
+-- The database name will be passed as an argument of the mysql command
+-- If the table "first_table" already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+	);
